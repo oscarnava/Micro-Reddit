88f7525b1fd863def715b0fb9f87a2ecfb1dc9cd
Mauricio Robayo <rfmajo@gmail.com>
2019-09-11 16:57:10 -0500

    Add comments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  has_many :comments
   belongs_to :user
-  validates :content, presence: true
+  validates :content, presence: true, length: { minimum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :comments
   has_many :posts
   validates :name, presence: true, length: { in: 5..30 }
 end
